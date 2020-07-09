dad7b2dfec2d66d0a608aa0a095cbec5ae1d7c08
mb2081 <alison.graham@gdit.hcqis.org>
2020-07-09 16:19:49 -0600

    Added a end point for posting a Task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,6 +9,11 @@ const port = process.env.PORT || 3000
 //Automatically parse incoming JSON to an object
 app.use(express.json())
 
+/*
+** CRUD operations Create Read Update Delete
+*/
+
+//CREATE
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -17,6 +23,18 @@ app.post('/users', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
