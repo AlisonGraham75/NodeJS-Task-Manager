f8c7cfa4df87c6654bd20d376ddce608e6d3c163
mb2081 <alison.graham@gdit.hcqis.org>
2020-07-10 14:00:50 -0600

    improved promis chaining demos with Async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,13 +3,27 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 //Demonstrate promise chaining.  
-//First get and update a user, then get a count of all users with age 1
-Task.findByIdAndRemove('5f077aee76bb5718453dcf94').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed:false}) 
-}).then ((result) => {
-    console.log(result)
-}).catch((e) => {
+//First get and delete a task, then get a count of all uncompleted tasks
+// Task.findByIdAndRemove('5f077aee76bb5718453dcf94').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed:false}) 
+// }).then ((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+//Demonstrate same as above using Async/Await
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+
+deleteTaskAndCount('5f07969305d1ab9cd9420c9c').then((count) => {
+    console.log(count)
+}).catch ((e) => {
     console.log(e)
 })
-
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,13 +2,27 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-//Demonstrate promise chaining.  
-//First get and update a user, then get a count of all users with age 1
-User.findByIdAndUpdate('5f0777f9098e4e19fcd45171', { age: 1}).then((user) =>{
-    console.log(user)
-    return User.countDocuments({ age:1 }) //Return the next promise
-}).then((result) => {
-    console.log(result)
-}).catch((e) => {
+// //Demonstrate promise chaining.  
+// //First get and update a user, then get a count of all users with age 1
+// User.findByIdAndUpdate('5f0777f9098e4e19fcd45171', { age: 1}).then((user) =>{
+//     console.log(user)
+//     return User.countDocuments({ age:1 }) //Return the next promise
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+//Below is same funcationality as above only it uses async/await pattern
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('5f0777f9098e4e19fcd45171', 2).then((count) => {
+    console.log(count)
+}).catch ((e) => {
     console.log(e)
 })
