c5c1fdedd7a810fa17da97462d1435c10d06f4ab
mb2081 <alison.graham@gdit.hcqis.org>
2020-07-10 07:35:19 -0600

    added 201 status codes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
@@ -28,7 +28,7 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
