62c46ac2ab6e37e0856bba249b7bbdef1286d935
mb2081 <alison.graham@gdit.hcqis.org>
2020-07-13 09:11:07 -0600

    Refactored Route Handlers to their own route files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,157 +10,8 @@ const port = process.env.PORT || 3000
 //Automatically parse incoming JSON to an object
 app.use(express.json())
 
-/*
-** CRUD operations Create Read Update Delete
-*/
-
-//CREATE
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-//READ
-app.get('/users', async (req, res) =>  {
-    try{
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-//READ 
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-   
-})
-
-//CREATE
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        await task.save()
-        res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-//READ
-app.get('/tasks', async (req, res) =>  {
-
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-//READ 
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-          res.status(500).send()
-    }
-   
-})
-
-//UPDATE
-app.patch('/users/:id', async (req, res) => {
-    
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    //every method runs the callback function onces for every item in an array
-    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'Invalid updates!'})
-    }
-    
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-//UPDATE
-app.patch('/tasks/:id', async (req, res) => {
-
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'Invalid updates!'})
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-//DELETE
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        return res.send(user)
-    } catch (e) {
-        res.status(500).send
-    }
-})
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        return res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
