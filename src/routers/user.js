f54132438e49f413c9ef0c0a7449cc7507f3b6a6
AlisonGraham75 <alison.graham10@gmail.com>
2020-07-20 11:47:55 -0600

    Added ability to upload images

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const authentication = require('../middleware/authentication')
 const router = new express.Router()
@@ -111,6 +112,23 @@ router.delete('/users/me', authentication, async (req, res) => {
     } catch (e) {
         res.status(500).send
     }
+})
+
+//UPLOAD
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
+})
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send(200)
 })
 
 module.exports = router
