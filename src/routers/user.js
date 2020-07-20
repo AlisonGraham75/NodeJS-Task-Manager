8299a406835aa6cfca139b2b798c1e751551c12e
AlisonGraham75 <alison.graham10@gmail.com>
2020-07-20 15:43:31 -0600

    Added endpoint for deleting avatar from user data

--- src/routers/user.js
+++ src/routers/user.js
@@ -114,6 +114,16 @@ router.delete('/users/me', authentication, async (req, res) => {
     }
 })
 
+router.delete('/users/me/avatar',authentication, async (req, res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send
+    }
+})
+
 //UPLOAD
 const upload = multer({
     limits: {
